cd4fe2dac2015a7b5d96578f4fab9cb18a0ce818
mcowell <matt@cowell.ca>
2020-09-29 15:09:02 -0400

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -4,11 +4,10 @@ import os
 execution_path = os.getcwd()
 
 detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "4.jpg"), output_image_path=os.path.join(execution_path , "image3new.jpg"), minimum_percentage_probability=30)
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
 
 for eachObject in detections:
-    print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
-    print("--------------------------------")
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
